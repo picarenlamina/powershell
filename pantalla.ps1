e5925e9c37b89f1194953107311a1da79bb78893
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2024-03-07 10:22:49 +0100

    Update pantalla.ps1

--- pantalla.ps1
+++ pantalla.ps1
@@ -1,6 +1,22 @@
-$path=(Get-Date).ToString("yyyyMMddhhmm")+"prueba.bmp"
-$b=New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
-$g=[System.Drawing.Graphics]::FromImage($b)
-$g.CopyFromScreen((New-Object System.Drawing.Point(0,0)), (New-Object System.Drawing.Point(0,0)), $b.Size)
-$g.Dispose()
-$b.Save($path,'Bmp')
+
+$i = 1
+while( $i )
+{ 
+    $file = (Get-Date).ToString("yyyyMMddhhmm")+".bmp"
+    $path="c:\temp\"
+    $b=New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
+    $g=[System.Drawing.Graphics]::FromImage($b)
+    $g.CopyFromScreen((New-Object System.Drawing.Point(0,0)), (New-Object System.Drawing.Point(0,0)), $b.Size)
+    $g.Dispose()
+    $b.Save($path + $file ,'Bmp')
+
+
+    $user = "u46609125-frm"
+    $password = "*******"
+
+    $client = New-Object System.Net.WebClient
+    $client.Credentials = New-Object System.Net.NetworkCredential($user, $password)
+    $client.UploadFile("ftp://ftp.iesjoseplanes.es/guardian/" + $file, $path + $file )
+
+    $i = $i + 1
+}
